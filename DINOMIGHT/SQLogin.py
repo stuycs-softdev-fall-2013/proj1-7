b933a2d840998374724f07e22502d743c43b47c3
Will <flyinbananatree@me.com>
2013-10-24 20:40:29 -0400

    SQLogin stuff

--- DINOMIGHT/SQLogin.py
+++ DINOMIGHT/SQLogin.py
@@ -4,8 +4,9 @@ import sqlite3
 userdata_filename = "keys.dat"
 secret_key = "uniquellama"
 
+#ensure db & table exist; should probably be fixed later
 connection = sqlite3.connect(userdata_filename)
-connection.execute("CREATE TABLE IF NOT EXISTS users(usern TEXT, pasw TEXT)")
+connection.execute("CREATE TABLE IF NOT EXISTS users(usern TEXT, passw TEXT)")
 connection.commit()
 connection.close()
 
@@ -13,14 +14,9 @@ def registerUser(usern, passw):
 	conn = sqlite3.connect(userdata_filename)
 
 	c = conn.cursor()
-
-
-	for user in result:
-		if user[0] == usern:
-			conn.close()
-			#the user already exists
-			return False
-
+	if (checkUser(usern, passw)):
+		conn.close()
+		return False
 	c.execute("insert into users values(?,?)", (usern, encrypt(passw)))
 	conn.commit()
 	conn.close()
@@ -35,18 +31,19 @@ def checkUser(usern,passw):
 	c = conn.cursor()
 
 	#c.execute("select * from users where ")
-	c.execute("select * from users where usern=(?)", usern)
-	for user in c:
-		if user[0] == usern:
-			ans = user[1] == encrypt(passw)
-			conn.close()
-
-			return ans
+	c.execute("select passw from users where usern=(?)", (usern,))
+	u = c.fetchone()
+	if u != None:
+		ans = u[0].encode('ascii') == encrypt(passw)
+		conn.close()		
+		return ans
 
 	return False
 
+# currently nonfunctional
 def changePass(usern, passw):
-	passw = passw.encode('ascii')
+	print "should change %s password to %s"%(usern, passw)
+	print "NOT YET IMPLEMENTED"
 	
 
 def encrypt(passw):
