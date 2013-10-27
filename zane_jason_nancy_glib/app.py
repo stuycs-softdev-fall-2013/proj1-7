cee2dab58e2f2dd69c70d310dad11d6015b45c8f
Zane Sterling <sterling.zane@gmail.com>
2013-10-27 17:37:36 -0400

    Improve structure of stories on home page.
    Give stories individualizable titles and lists of lines.
    Make stories displayed as:
    
    <h4>Title</h4>
    <well>
    	line
    	line
    	...
    </well>

--- zane_jason_nancy_glib/app.py
+++ zane_jason_nancy_glib/app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def home():
-	d = {}
-	d['stories'] = ["","",""]
+	d = {'stories': []}
+	story = ["Title", "Line.", "Another line.", "A third line."]
+	for i in range(3):
+		d['stories'].append(story)
+
 	if request.method == "GET":
 		return render_template("home.html", d=d)
 	else:
