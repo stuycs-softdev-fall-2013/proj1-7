6ddd43ea98d9b9f4c01659127faf87ee70125f51
Christopher Burke <cburke496@gmail.com>
2013-10-27 13:08:42 -0400

    Added datagrab skeleton file

---
+++ christine_josh_christopher_sebastian/datagrab.py
@@ -0,0 +1,38 @@
+def addUser(username, password):
+    #used in the register page to create accounts
+    pass
+
+
+def authenticate(username, password):
+    #user when logging in to verify account info
+    pass
+
+
+def plusUserKarma(targetUser, sourceUser):
+    #sourceUser is adding one to the karma of targetUser
+    pass
+
+
+def minusUserKarma(targetUser, sourceUser):
+    #sourceUser is subtracting one from the karma of targetUser
+    pass
+
+
+def plusStoryKarma(storyID, user):
+    #user is adding one to the karma of the story with the ID storyID
+    pass
+
+
+def minusStoryKarma(storyID, user):
+    #user is subtracting one from the karma of the story with the ID storyID
+    pass
+
+
+def newEdit(storyID, user):
+    #user adds a new edit to the story with ID storyID
+    pass
+
+
+def newStory(storyID, user):
+    #user forks the story with ID storyID
+    pass
