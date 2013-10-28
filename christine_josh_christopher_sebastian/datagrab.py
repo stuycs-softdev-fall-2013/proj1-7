7f83b39f9c83eef72d49d754e46f39cbfde2f697
Christopher Burke <cburke496@gmail.com>
2013-10-28 12:44:14 -0400

    Started with dislikeEdit

--- christine_josh_christopher_sebastian/datagrab.py
+++ christine_josh_christopher_sebastian/datagrab.py
@@ -43,14 +43,24 @@ def likeEdit(editID, sourceUser):
     q = "select liked_edits from account_info where username=?"
     cursor = connection.execute(q, [sourceUser])
     likedEdits = [line for line in cursor]
+    q = "select disliked_edits from account_info where username=?"
+    cursor = connection.execute(q, [sourceUser])
+    dislikedEdits = [line for line in cursor]
     if likedEdits[0][0] != '':
         likedEdits = likedEdits[0][0].split(',')
         for edit in likedEdits:
             if edit == "%i"%(editID):
                 return False
         updatedLikedEdits = ",".join(likedEdits) + ",%i"%(editID)
-    else:
+    else:        
         updatedLikedEdits = "%i"%(editID)
+
+    if dislikedEdits[0][0] != '':
+        dislikedEdits = dislikedEdits[0][0].split(',')
+        for edit in dislikedEdits:
+            if edit == "%i"%(editID):
+                return False
+            
     q = "select karma from account_info where username=?"
     cursor = connection.execute(q, [targetUser])
     currentKarma = [line for line in cursor]
@@ -66,7 +76,45 @@ def dislikeEdit(editID, sourceUser):
     #sourceUser is subtracting one from the karma of edit with editID
     #returns False if a user tries to dislike their own edit or dislike an edit twice
     connection = sqlite3.connect('OnceUponData.db')
-    pass
+    q = "select user from edits where id=?"
+    cursor = connection.execute(q, [editID])
+    targetUser = [line for line in cursor]
+    targetUser = targetUser[0][0].encode('ascii','inore')
+
+    if targetUser == sourceUser:
+        return False
+    q = "select liked_edits from account_info where username=?"
+    cursor = connection.execute(q, [sourceUser])
+    likedEdits = [line for line in cursor]
+    q = "select disliked_edits from account_info where username=?"
+    cursor = connection.execute(q, [sourceUser])
+    dislikedEdits = [line for line in cursor]
+
+    
+    if likedEdits[0][0] != '':
+        likedEdits = likedEdits[0][0].split(',')
+        for edit in likedEdits:
+            if edit == "%i"%(editID):
+                return False
+
+    if dislikedEdits[0][0] != '':
+        dislikedEdits = dislikedEdits[0][0].split(',')
+        for edit in dislikedEdits:
+            if edit == "%i"%(editID):
+                return False
+        updatedDislikedEdits = ",".join(dislikedEdits) + ",%i"%(editID)
+    else:
+        updatedDislikedEdits = "%i"%(editID)
+
+    q = "select karma from account_info where username=?"
+    cursor = connection.execute(q, [targetUser])
+    currentKarma = [line for line in cursor]
+    q = "update account_info set karma=? where username=?"
+    connection.execute(q, currentKarma[0][0]-1, [targetUser])
+    q = "update account_info set disliked_edits=? where username=?"
+    connection.execute(q, [updatedDislikedEdits, sourceUser])
+    connection.commit()
+    return True
 
 
 def likeStory(storyID, user):
@@ -75,7 +123,7 @@ def likeStory(storyID, user):
     pass
 
 
-def dislikeEdit(storyID, user):
+def dislikeStory(storyID, user):
     #user is subtracting one from the karma of the story with the ID storyID
     connection = sqlite3.connect('OnceUponData.db')
     pass
