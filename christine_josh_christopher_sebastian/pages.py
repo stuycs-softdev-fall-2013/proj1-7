f8cad9000d5019fbb77e6d2198a033ad731f776c
Josh Hofing <joshhofing@gmail.com>
2013-10-28 12:20:44 -0400

    Add an interface for most of the pages

--- christine_josh_christopher_sebastian/pages.py
+++ christine_josh_christopher_sebastian/pages.py
@@ -1,4 +1,52 @@
-from flask import render_template
+from flask import Flask, render_template
 
-def homepage():
-    return render_template('home.html');
+app = Flask(__name__)
+
+@app.route('/')
+def homepage_loggedout():
+    stories = [
+            {
+                "id":1234,
+                "title":"Banana",
+                "description":"Killer Bananas Attack Things"
+            },
+            {
+                "id":5634,
+                "title":"Once Upon a Time...",
+                "description":"Once Upon a Time, in the magical land of..."
+            },
+            {
+                "id":1237,
+                "title":"The End of Time",
+                "description":"Doom approaches..."
+            },
+            {
+                "id":1238,
+                "title":"Blah Blah",
+                "description":"blah"
+            }
+            ]
+    return render_template('home.html', stories=stories);
+
+def homepage_loggedin():
+    pass
+
+def logout():
+    pass
+
+def login():
+    pass
+
+def register():
+    pass
+
+def stories(storyid):
+    pass
+
+def profile(userid):
+    pass
+
+
+if __name__ == '__main__':
+    app.debug = True;
+    app.run();
