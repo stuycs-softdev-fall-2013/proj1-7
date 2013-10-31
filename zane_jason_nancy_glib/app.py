c88a1e5e03ad0c66233ce426b48311c57c642cf9
Nancy Dai <babycarr6193@gmail.com>
2013-10-31 07:44:03 -0400

    zane check code in proj

--- zane_jason_nancy_glib/app.py
+++ zane_jason_nancy_glib/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "secret key"
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -13,14 +14,18 @@ def home():
 	if request.method == "GET": #GET
 		return render_template("home.html", d=d)
 	else: #POST
+		session['user'] = "user"
 		d['loggedIn'] = True
 		return render_template("home.html", d=d)
 
 @app.route("/account")
 def account():
-	return render_template("account.html")
+	if (session['user'] == "user"):
+		d = {}
+		d['loggedIn'] = True
+	return render_template("account.html", d=d)
 
 
 if __name__ == "__main__":
 	app.debug = True
-	app.run()
+	app.run(port = 5005)
