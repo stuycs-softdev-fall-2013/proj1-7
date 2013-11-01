142f747e8b4621bf161c76b549056838b2d63477
Nicholas Galasinao <nicholas.galasinao@gmail.com>
2013-11-01 12:47:57 -0400

    more comments

--- josh_nick_justin_shaan/flask.py
+++ josh_nick_justin_shaan/flask.py
@@ -18,23 +18,50 @@ def login():
 @app.route("/register")
 def register():
     # register user
+    # create user page
     pass
 
 @app.route("/index")
 def index():
     # list of blog posts
+    # link to each post
     pass
 
 @app.route("post1")
 def post1():
     # look at post 1
+    # load comments
+    pass
+
+@app.route("post2")
+def post2():
+    # look at post 2
+    # load comments
+    pass
+
+@app.route("post3")
+def post3():
+    # look at post 3
+    # load comments
     pass
 
 @app.route("/writepost")
 def writepost():
-    # if admin, create a new post
-    # else, redirect somewhere
+    # if logged in, create a new post
+    # else, redirect to login
     pass
+
+@app.route("/deletepost")
+def deletepost():
+    # if logged in, delete your own posts
+    # if admin, delete any post
+    # else, redirect to login
+    pass
+
+@app.route("/userpage")
+def userpage():
+    # if logged in, display your username, posts, and comments
+    # else, redirect to login
     
 if __name__ == "__main__":
     app.debug=True
