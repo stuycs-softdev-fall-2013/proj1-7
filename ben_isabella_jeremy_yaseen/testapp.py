ed817f3c0126d633bc7e59f9a3b341b13b277b63
Yaseen <yislam96@gmail.com>
2013-11-03 10:39:12 -0500

    index.html edit, login.html edit, signup layout was wrong - fixed

--- ben_isabella_jeremy_yaseen/testapp.py
+++ ben_isabella_jeremy_yaseen/testapp.py
@@ -10,6 +10,14 @@ u={"username":"YaseenMe"}
 def home():
     return render_template("login.html")
 
+@app.route("/")
+def hoome():
+    return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
