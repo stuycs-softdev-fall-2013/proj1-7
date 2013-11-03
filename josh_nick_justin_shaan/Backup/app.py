d8eccaafd3056d90bf2af2b9f62211f371629821
Joshua Chin <joshuarchin@gmail.com>
2013-11-02 23:41:05 -0400

    fleshed out app.py

--- josh_nick_justin_shaan/Backup/app.py
+++ josh_nick_justin_shaan/Backup/app.py
@@ -1,22 +1,38 @@
 import flask
-
 from flask import Flask
-app = Flask(__name__)
-
+from flask import redirect
+from flask import render_template
+from flask import url_for
 from post_database import PostDatabase
+
+app = Flask(__name__)
 posts_db = PostDatabase('posts.db')
 
 @app.route('/')
+@app.route('/index.html')
 def index():
-    return str(posts_db.get_posts())
+    return render_template(
+        'index.html'
+        posts_db.get_posts())
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'login'
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        pass #AUTHENTICATE
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 def admin():
-    return 'admin'
+    if session['username'] != 'admin':
+        return redirect(url_for('index'))
+    if request.method == 'GET':
+        return render_template('admin.html')
+    elif request.method == 'POST':
+        posts_db.add_post(
+            request.form['title'],
+            request.form['content'])
+        return render_template('admin.html')
 
 def main():
     app.debug = True
