c81565d2f3f0a4161b0bdee9a4b5926b2f73b175
Yaseen <yislam96@gmail.com>
2013-11-03 14:41:20 -0500

    working home page, finally

--- ben_isabella_jeremy_yaseen/testapp.py
+++ ben_isabella_jeremy_yaseen/testapp.py
@@ -6,17 +6,21 @@ app = Flask(__name__)
 
 u={"username":"YaseenMe"}
 
+_post={"title":"TITLE","user":"YaseenMe","date":"11/3/13 14:39:22","upvotes":52,"body":"THis is the body of the post"}
+
+posts=[_post]
+
 @app.route("/login")
 def home():
-    return render_template("login.html", error="badlogin")
+    return render_template("login.html")
 
 @app.route("/")
 def hoome():
-    return render_template("index.html")
+    return render_template("index.html",posts=posts)
 
 @app.route("/register")
 def signup():
-    return render_template("register.html", error="alreadyregistered")
+    return render_template("register.html")
 
 
 if __name__ == '__main__':
