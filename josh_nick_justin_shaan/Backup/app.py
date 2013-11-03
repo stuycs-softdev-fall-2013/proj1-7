e3c4616c039ed5942b7d133474743fb9544d062a
Joshua Chin <joshuarchin@gmail.com>
2013-11-03 00:08:24 -0400

    updated app.py to use authentication database

--- josh_nick_justin_shaan/Backup/app.py
+++ josh_nick_justin_shaan/Backup/app.py
@@ -4,9 +4,11 @@ from flask import redirect
 from flask import render_template
 from flask import url_for
 from post_database import PostDatabase
+from auth_database import AuthDatabase
 
 app = Flask(__name__)
 posts_db = PostDatabase('posts.db')
+auth_db = AuthDatabase('auth.db')
 
 @app.route('/')
 @app.route('/index.html')
@@ -20,7 +22,12 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
-        pass #AUTHENTICATE
+        if auth_user(
+            request.form['username'],
+            request.form['password']):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        return render_template('login.html')
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
