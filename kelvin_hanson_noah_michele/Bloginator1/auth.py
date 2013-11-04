c8d8000a2b2b004b8c990c59e7a77f410185ea3f
Michele <michelelin123@hotmail.com>
2013-11-04 17:33:56 -0500

    Added original copy

---
+++ kelvin_hanson_noah_michele/Bloginator1/auth.py
@@ -0,0 +1,52 @@
+#!usr/bin/python
+
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.KMNH
+users = db.users
+posts = db.posts
+comments = db.comments
+
+def addUser(usr,pwd, admin=False):
+    if(usr==None or pwd==None):
+        return False
+    if users.find({'usr':usr}).count() == 0:
+        users.insert({'usr':usr,'pwd':pwd, 'admin':admin})
+        return True
+    else:
+        return False
+
+def auth(usr,pwd):
+    if(usr==None or pwd==None):
+        return False
+    if users.find({'usr':usr}).count() == 0:
+        return False
+    if users.find_one({'usr':usr})['pwd'] == pwd:
+        return True
+    return False
+
+def reset(usr, oldpwd, newpwd):
+    if auth(usr, oldpwd) == False and newpwd != None:
+        users.find_one({'usr':usr}).upsert({'pwd':newpwd})
+        return True
+    return False
+
+def isAdmin(usr):
+    if not exists(usr): return False
+    return users.find_one({'usr':usr})['admin']
+
+def exists(usr):
+    if (usr == None or 
+        users.find({'usr':usr}).count()) == 0:
+        return False
+    return True
+
+
+
+if(__name__ == "__main__"):
+    
+    print addUser("user", "password")
+    print "%s (true)"%auth('user','password')
+    print "%s (false)"%auth('user','asdf')
+    print "%s (false)"%auth('asdf','asdf')
---
+++ kelvin_hanson_noah_michele/Bloginator1/comments.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+from datetime import timedelta
+
+client = MongoClient()
+db = client.KMNH
+users = db.users
+posts = db.posts
+comments = db.comments
+
+def addComment(usr,post_id,comment,timestamp):
+    if usr == None:
+        usr = "guest"        
+    if(post_id==None or comment==None):
+        return False
+    comments.insert({'usr':usr,'post_id':post_id,'comment':comment,'timestamp':datetime.datetime.now()})
+    
+
+def editComment(usr,comment,newcomment):
+    if comments.find({'usr':usr,'comment':comment}).count != 0 and newcomment != None:
+        comments.find_one({'usr':usr,'comment':comment}).upsert({'comment':newcomment})
+        return True
+    return False
+
+def removeComment(usr,comment):
+    if comments.find({'usr':usr,'comment':comment}).count != 0:
+        comments.remove({'usr':usr,'comment':comment},1)
+        return True
+    return False
+                                            
+def getComments(usr,post):
+    if comments.find({'usr':usr,'title':title}).count != 0:
+        return comments.find({'usr':usr,'title':title})
+    return False
+                                            
