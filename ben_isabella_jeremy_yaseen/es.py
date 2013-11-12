9bf54286ce1d0522b2607bdaf096e4d28efb212f
Benjamin Attal <attal.benjamin@gmail.com>
2013-11-12 18:56:41 -0500

    put elasticsearch in the app

--- ben_isabella_jeremy_yaseen/es.py
+++ ben_isabella_jeremy_yaseen/es.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 from elasticsearch import Elasticsearch
 from pymongo import MongoClient
+from bson import ObjectId
 from models import Post
 from settings import ES_REPEAT
 import sched, time
@@ -15,23 +16,21 @@ if not es.indices.exists('bloginator'):
 
 def index():
     ids = []
-    print "indexing..."
     for p in posts.find():
 	ids.append(p.get_id())
 	p._obj.pop('_id', None)
         es.index(index='bloginator', doc_type='post', id=p._id, body=p._obj)
 
-    print "pruning..."
     query = {'query': {'match_all': {}}}
     results = es.search(index='bloginator', doc_type='post', body=query)
     for p in results['hits']['hits']:
-	if p['_id'] not in ids:
+	if ObjectId(p['_id']) not in ids:
 	    es.delete('bloginator', doc_type='post', id=p['_id'])
     index_task.enter(ES_REPEAT, 1, index, ())
 
 
 def search(keyword):
-    query = {'query': {'term': {'_all': keyword}}}
+    query = {'query': {'fuzzy': {'_all': keyword}}}
     results = es.search(index='bloginator', doc_type='post', body=query)
     for p in results['hits']['hits']:
         p['_source']['_id'] = p['_id']
